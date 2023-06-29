2d74bec3768c0df9bf8732f4324d768d472ba896
Dmitry <badl1970@mail.ru>
2023-06-29 23:14:26 +0300

    fixed bugs in Task_02

--- src/main/java/Homework_2/Task_02.java
+++ src/main/java/Homework_2/Task_02.java
@@ -15,12 +15,15 @@ public class Task_02 {
     public static void main(String[] args) {
 
         int[] intArray = new int[] {1,4,7,9,3,6,8,45,23,67};
-
+        int d = 0;
         try {
-            double d = 5;  // надо было исправить int на double, иначе результат округляется до целых значений
-            double catchedRes1 = intArray[8] / d;
-            System.out.println("catchedRes1 = " + catchedRes1);
-        } catch (ArithmeticException e) {
+            if (d != 0) {
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } else {
+                throw new ArithmeticException();
+            }
+        }catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
         }
     }
